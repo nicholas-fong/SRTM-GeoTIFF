7e6632a38f53676d4c9cc73881616bc2ac67ee1d
Nicholas Fong <nicholas.fong@gmail.com>
2021-02-18 16:45:31 -0800

    Update srtm1.py
    added comments about gdal GetGeoTransform

--- library/srtm1.py
+++ library/srtm1.py
@@ -4,15 +4,15 @@ def read(my_file, lat, lon):
     data = gdal.Open(my_file)
     band1 = data.GetRasterBand(1)
     GT = data.GetGeoTransform()
-    # call gdal's Affine Transformation (GetGeoTransform method)
+    # gdal's Affine Transformation (GetGeoTransform)
     # GetGeoTransform translates latitude, longitude to pixel indices
-    x_pixel_size = GT[1]
-    y_pixel_size = GT[5]
+    # GT[0] and GT[3] defines the "origin": top left corner pixel 
+    x_pixel_size = GT[1]    #horizontal pixel size
+    y_pixel_size = GT[5]    #vertical pixel size
     xP = int((lon - GT[0]) / x_pixel_size )
     yL = int((lat - GT[3]) / y_pixel_size )
+    # without rotation, GT[2] and GT[4] are zero. 
     return ( int( band1.ReadAsArray(xP,yL,1,1) ) )
 
-# gdal Affine GetGeoTransform interprets different file formats, tile sizes and resolutions
 # File formats supported: GeoTIFF, DTED, HGT, BIL
-# Data sources: USGS EarthExplorer, CGIAR-CSI, NASA ASTER GDEM, OpenTopography, GMTED2010
-
+# Data sources: USGS EarthExplorer, CGIAR-CSI, NASA ASTER GDEM, OpenTopography, ALOS World 3D, USGS GMTED2010 (wide area)
