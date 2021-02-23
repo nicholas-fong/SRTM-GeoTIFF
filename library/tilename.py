8ca9d14b7b3d3e4f891d1f57fac0a39dab26e17f
Nicholas Fong <nicholas.fong@gmail.com>
2021-02-23 07:53:05 -0800

    Updated comments

--- library/tilename.py
+++ library/tilename.py
@@ -1,9 +1,9 @@
-# determine file name of GeoTIFF file based on latitude longitude
-# For square tiles: 1 degree x 1 degree, 1 pixel overlap. 
+# Determine file name of GeoTIFF file based on latitude longitude
+# For square tiles with 1 pixel overlap. Not for square tiles with non-overlapping pixels.
 # Minor style difference between NASA ASTER and USGS EarthExplorer
 # NASA ASTER use N E S W; USGS use lowercase n e s w
 # NASA does not use _ in the file name, USGS uses _ as a separator
-
+# returned file name is made to look the same as traditional .HGT except .TIF
 
 import math
 
