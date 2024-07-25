5118fb94278346fa7b1979fed1a9e57e8b7b42a0
Nicholas Fong <nicholas.fong@gmail.com>
2024-07-24 20:36:15 -0700

    Update gpx2elev.py

--- library/gpx2elev.py
+++ library/gpx2elev.py
@@ -26,52 +26,50 @@ def find ( latitude, longitude ):
         t2 = f"{math.floor(longitude):03d}"
     return( f"../geotiff/{hemi}{t1}{meri}{t2}.tif" ) 
 
-def extract (tiff_file, lat, lon):
+def extract_elevation(tiff_file, lat, lon):
     gdal.UseExceptions()
-    data = gdal.Open(tiff_file) 
-    band1 = data.GetRasterBand(1)
-    GT = data.GetGeoTransform()
-    # GDAL's Affine Transformation (GetGeoTransform) 
-    # https://gdal.org/tutorials/geotransforms_tut.html
-    # GetGeoTransform translates latitude, longitude to pixel indices
-    # GT[0] and GT[3] define the "origin": upper left pixel 
-    x_pixel_size = GT[1]    #horizontal pixel size
-    y_pixel_size = GT[5]    #vertical pixel size
-    xP = int((lon - GT[0]) / x_pixel_size )
-    yL = int((lat - GT[3]) / y_pixel_size )
-    # without rotation, GT[2] and GT[4] are zero
-    array_result = band1.ReadAsArray(xP, yL, 1, 1)
-    single_element = array_result[0, 0]  # Extract a single element
-    integer_value = int(single_element)  # Convert to an integer
-    return (integer_value)
-            
+    try:
+        data = gdal.Open(tiff_file)
+        if data is None:
+            print(f"Failed to open file: {tiff_file}")
+            return 0        
+        band1 = data.GetRasterBand(1)
+        GT = data.GetGeoTransform()
+        x_pixel_size = GT[1]
+        y_pixel_size = GT[5]
+        xP = int((lon - GT[0]) / x_pixel_size)
+        yL = int((lat - GT[3]) / y_pixel_size)
+        array_result = band1.ReadAsArray(xP, yL, 1, 1)
+        single_element = array_result[0, 0]
+        integer_value = int(single_element)
+        return integer_value    # return elevation found in GeoTIFF tile
+    except Exception as e:
+        print(f"Error extracting altitude: {e}")
+        return 0
 # read a gpx file and append elevation tags to waypoints, route points and track points
 
 with open( sys.argv[1]+'.gpx', 'r', encoding='utf-8') as infile:
     gpx = gpxpy.parse(infile)
-infile.close()
 
 for p in gpx.waypoints:
     tiff_file=find(p.latitude, p.longitude)
-    z = extract (tiff_file,p.latitude,p.longitude)
+    z = extract_elevation (tiff_file,p.latitude,p.longitude)
     p.elevation = z
 
 for routes in gpx.routes:
     for p in routes.points:
         tiff_file=find(p.latitude, p.longitude)
-        z = extract (tiff_file,p.latitude,p.longitude)
+        z = extract_elevation (tiff_file,p.latitude,p.longitude)
         p.elevation = z
 
 for track in gpx.tracks: 
     for segment in track.segments:
         for p in segment.points:
             tiff_file=find(p.latitude, p.longitude)
-            z = extract (tiff_file,p.latitude,p.longitude)
+            z = extract_elevation (tiff_file,p.latitude,p.longitude)
             p.elevation = z
 
 #print (gpx.to_xml())
 with open(sys.argv[1]+'.gpx', 'w') as file:
     file.write( gpx.to_xml() )
-file.close()
-
-    
+print ( f"File with elevation saved as {sys.argv[1]+'.gpx'}")    
